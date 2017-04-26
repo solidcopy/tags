f886d509af5e4471eb5a5af4ccc9a296b5572dfe
yuu <dev@derevijargon.jp>
2017-04-26 21:02:12 +0900

    仮想環境に対応。

--- bin/tags.bat
+++ bin/tags.bat
@@ -2,4 +2,13 @@
 set AUDIO_FILES_HOME=%PROJECTS_HOME%\audio_files
 set PYTHONPATH=%TAGS_HOME%\src;%AUDIO_FILES_HOME%\src
 @echo on
+
+@echo off
+call %TAGS_HOME%\bin\env\Scripts\activate.bat
+@echo on
+
 python %TAGS_HOME%\src\tags\main.py %1
+
+@echo off
+call %TAGS_HOME%\bin\env\Scripts\deactivate.bat
+@echo on
