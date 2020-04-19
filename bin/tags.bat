1b8c16682f37d81912210f47773d72f736c54d57
yuu <fmck@solidcopy.net>
2020-04-19 09:56:28 +0900

    tags.batを更新。

--- bin/tags.bat
+++ bin/tags.bat
@@ -1,14 +1,12 @@
 @echo off
 set AUDIO_FILES_HOME=%PROJECTS_HOME%\audio_files
-set PYTHONPATH=%TAGS_HOME%\src;%AUDIO_FILES_HOME%\src
+set TAGS_HOME=%PROJECTS_HOME%\tags
+set PYTHONPATH=%PYTHONPATH%;%TAGS_HOME%\src;%AUDIO_FILES_HOME%\src
+call %TAGS_HOME%\env\Scripts\activate.bat
 @echo on
 
-@echo off
-call %TAGS_HOME%\bin\env\Scripts\activate.bat
-@echo on
-
-python %TAGS_HOME%\src\tags\main.py %1
+python -m tags %1
 
 @echo off
-call %TAGS_HOME%\bin\env\Scripts\deactivate.bat
+call %TAGS_HOME%\env\Scripts\deactivate.bat
 @echo on
